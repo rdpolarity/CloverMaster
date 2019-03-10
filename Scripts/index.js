26e1bd1f4ebf6a4951880b8a3c6813d82e775129
rdpolarity <rdpolarity@gmail.com>
2019-03-10 16:00:20 +1100

    Initial Commit

---
+++ Scripts/index.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('loading'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('desktop'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'deskdata.json'
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+# CommishList Website Master
+Welcome to the startup project for CommishList, a multi platform commission tracking tool.
