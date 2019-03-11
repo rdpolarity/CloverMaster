6367b338c4670ee8be33c78a2ab3d887904037cf
rdpolarity <rdpolarity@gmail.com>
2019-03-11 15:22:33 +1100

    Fixed loading screen

--- Scripts/loading.js
+++ Scripts/loading.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    $('.preloader-background').delay(1700).fadeOut('slow');
-
-    $('.preloader-wrapper')
-      .delay(1700)
-      .fadeOut();
-  });
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(1700).fadeOut('slow');
+	
+	$('.preloader-wrapper')
+		.delay(1700)
+		.fadeOut();
+});
